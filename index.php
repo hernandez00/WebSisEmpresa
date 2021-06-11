368ca50992cd4a91075bc1dca909541aaaf14f19
Leonardo Hernadnez <hernandeez00@gmail.com>
2021-06-11 20:06:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" type="text/css" href="_css/_fontawesome/css/all.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Websisitb</title>
+<title>Websis</title>
 
 <script src="_js/_jquery/_plugins/jquery-3.5.1.min.js"></script>
 <script src="_js/_jquery/_plugins/mask-plugin-master/src/jquery.mask.js"></script>
